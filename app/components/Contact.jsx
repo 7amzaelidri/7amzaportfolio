1a681c6293af3c003c0afb16ff65ca959ea14521
Thomas-Horvath <thomas.horvath0416@gmail.com>
2025-06-07 13:30:05 +0200

    dark mode, contact section

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -2,7 +2,7 @@ import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React , { useState } from 'react'
 
-const Contact = () => {
+const Contact = ( ) => {
      const [result, setResult] = useState("");
 
     const onSubmit = async (event) => {
@@ -32,7 +32,7 @@ const Contact = () => {
 
     return (
         <div className='w-full px-[12%] py-10 scroll-mt-20 min-h-screen 
-        bg-[url("/footer-bg-color.png")] bg-no-repeat bg-[length:90%_auto]' id='contact'>
+        bg-[url("/footer-bg-color.png")] bg-no-repeat bg-[length:90%_auto] dark:bg-none' id='contact'>
             <h4 className='text-center mb-2 text-lg font-Ovo'>Connect with me</h4>
             <h2 className='text-center  text-5xl font-Ovo'>Get in touch</h2>
 
@@ -42,20 +42,20 @@ const Contact = () => {
             </p>
 
 
-            <form className='max-w-2xlmx-auto' onSubmit={onSubmit}>
+            <form className='max-w-2xl mx-auto' onSubmit={onSubmit}>
                 <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
                     <input type="text" placeholder="Enter your Name" required
-                        className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='name'/>
+                        className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='name'/>
                     <input type="email" placeholder="Enter your Email" required
-                        className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='email'/>
+                        className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='email'/>
                 </div>
 
 
                 <textarea rows='6' placeholder="Enter your Message" required    
-                    className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6' name='message'
+                    className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90' name='message'
                 ></textarea>
                 <button type='submit'
-                    className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500'
+                    className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover'
                 >
                     Send Message
                     <Image src={assets.right_arrow_white} alt='right arrow ' className='w-4' />
