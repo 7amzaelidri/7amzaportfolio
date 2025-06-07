254bb7f614e89eff70e0a169e94d18cb37675850
Thomas-Horvath <thomas.horvath0416@gmail.com>
2025-06-07 13:22:59 +0200

    dark mode, portfolio section

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -2,7 +2,7 @@ import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-export const Work = () => {
+export const Work = ({isDarkMode}) => {
     return (
         <div id='work' className='w-full px-[12%] py-10 scroll-mt-20 min-h-screen'>
             <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
@@ -14,7 +14,7 @@ export const Work = () => {
             </p>
 
 
-            <div className="grid grid-cols-auto my-10 gap-5">
+            <div className="grid grid-cols-auto my-10 gap-5 dark:text-black">
                 {workData.map((project, index) => (
                     <div key={index}
                         className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
@@ -38,9 +38,9 @@ export const Work = () => {
 
             <a href="" className='w-max flex items-center justify-center gap-2
             text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 
-            mx-auto my-20 hover:bg-lightHover duration-500' >
+            mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover' >
                 Show more
-                <Image src={assets.right_arrow_bold} alt='arrow' className='w-4' />
+                <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='arrow' className='w-4' />
             </a>
 
 
