e34005dac90b6bd2d9819e3d83414bff3068a13a
Thomas-Horvath <thomas.horvath0416@gmail.com>
2025-06-08 16:11:29 +0200

    adding mention animation, header section

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,42 +1,74 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Header = () => {
     return (
         <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4' id='top'>
-            <div>
+            <motion.div
+                initial={{ scale: 0 }}
+                whileInView={{ scale: 1 }}
+                transition={{ duration: 0.5, type: 'spring', stiffness: 100 }}
+
+            >
                 <Image src={assets.profile_img} alt='profile imgage' className='rounded-full w-32'></Image>
-            </div>
-            <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Hi! I'm William Mark
+            </motion.div>
+            <motion.h3
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6, delay: 0.3 }}
+
+                className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Hi! I'm William Mark
                 <Image src={assets.hand_icon} alt='hand icon' className='w-6'></Image>
-            </h3>
-            <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo ' >frontend web developer based in London.</h1>
-            <p className='max-w-2xl mx-auto font-Ovo'>
+            </motion.h3>
+            <motion.h1
+                initial={{ opacity: 0,  y: -30 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, delay: 0.3 }}
+                className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo ' >frontend web developer based in London.</motion.h1>
+            <motion.p
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6, delay: 0.3 }}
+
+                className='max-w-2xl mx-auto font-Ovo'>
                 I'm a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.
-            </p>
+            </motion.p>
 
             <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-                <a href="#contact"
-                className='px-10 py-3 border
-                 border-white 
-                 rounded-full
+                <motion.a
+                    initial={{ opacity: 0, y: 30 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.6, delay: 1 }}
+
+
+                    href="#contact"
+                    className='px-10 py-3 border
+                  border-white 
+                    rounded-full
                   bg-black
                    text-white 
-                   flex 
-                   items-center 
-                   gap-2 
-                   dark:bg-transparent
-                   '
+                    flex 
+                    items-center 
+                     gap-2 
+                     dark:bg-transparent
+                    '
                 >Contact me
                     <Image src={assets.right_arrow_white} alt='right arrow' className='w-4'></Image>
-                </a>
+                </motion.a>
 
-                <a href="/sample-resume.pdf" download 
-                className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'
-                >my resume
+                <motion.a
+                    initial={{ opacity: 0, y: 30 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.6, delay: 1.2 }}
+
+                    href="/sample-resume.pdf" download
+                    className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'
+                >
+                    my resume
                     <Image src={assets.download_icon} alt='download icon' className='w-4'></Image>
-                </a>
+                </motion.a>
             </div>
         </div>
     )
